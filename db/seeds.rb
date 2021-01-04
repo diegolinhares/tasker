846e499f163d6e5ad7a5e08a174b84c60af23382
Diego Linhares <diegomlinhares@gmail.com>
2021-01-04 01:49:42 -0300

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+list = List.create(name: Faker::Hipster.sentence)
+
+10.times do
+  list.tasks.create(
+    name: Faker::Hipster.sentence,
+    completed_at: Faker::Boolean.boolean(true_ratio: 0.2) ? Time.current : nil
+  )
+end
