048f30deacf8acbf733177105f0de457e9113c65
Diego Linhares <diegomlinhares@gmail.com>
2021-01-31 16:27:34 -0300

    Complete a task

---
+++ app/javascript/controllers/task_controller.js
@@ -0,0 +1,10 @@
+import { Controller } from "stimulus"
+import StimulusReflex from "stimulus_reflex"
+
+export default class extends Controller {
+  connect() {
+    // This line communicates the client with the server.
+    // It gives a cable
+    StimulusReflex.register(this)
+  }
+}
