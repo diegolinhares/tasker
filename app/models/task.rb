3a864566b99e8b3a87f0ca4281641f8baa61753b
Diego Linhares <diegomlinhares@gmail.com>
2021-02-01 22:11:34 -0300

    Add users to the application

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,8 @@
 class Task < ApplicationRecord
   belongs_to :list
+
+  belongs_to :creator, class_name: 'User'
+  belongs_to :assignee, class_name: 'User', optional: true
   
   acts_as_list scope: [:list_id, completed_at: nil], top_of_list: 0
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.safe_email,
+  password: 'password',
+  password_confirmation: 'password'
+)
 
 list = List.create(name: Faker::Hipster.sentence)
 
 10.times do
   list.tasks.create(
     name: Faker::Hipster.sentence,
-    completed_at: Faker::Boolean.boolean(true_ratio: 0.2) ? Time.current : nil
+    completed_at: Faker::Boolean.boolean(true_ratio: 0.2) ? Time.current : nil,
+    creator: user
   )
 end
